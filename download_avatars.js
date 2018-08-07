75ee2a57d5e3175ad0dfd69dc453f5550f5e079b
Neenus Gabriel <neenus@live.ca>
2018-08-07 00:38:53 +0000

    data being received

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,27 @@
 var request = require('request');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
+
+function getRepoContributors(repoOwner, repoName, cb) { 
+    var options = {
+        uri: 'https://api.github.com',
+        path:  '/repos/' + repoOwner + '/' + repoName + '/contributors'
+    }; 
+    const auth = {
+        url: options.uri + options.path,
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+    request.get(auth, function(err, result) {
+        cb(err, result);
+    });
+
+}
+
+getRepoContributors('jquery', 'jquery', function(errors, result) {
+    console.log("Errors:", errors);
+    console.log("Result:", result);
+
+  });
+
